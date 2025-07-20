c5c83fca7bd9d3ea299c0ae14f4f06487d0aafe9
Kwonyeojiny <kangagi0009@naver.com>
2025-07-20 16:58:28 +0900

    feat: 로딩 상태 추가

--- src/components/MatZipList.tsx
+++ src/components/MatZipList.tsx
@@ -5,15 +5,30 @@ import type { MatZip } from "../types/MatZip";
 
 const MatZipList = () => {
   const [lists, setLists] = useState<MatZip[]>([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const getMapZipList = async () => {
-      const data = await getMatZipPlace();
-      setLists(data);
+      try {
+        const data = await getMatZipPlace();
+        setLists(data);
+      } catch (error) {
+        console.error("맛집 목록을 가져오는 데 실패했습니다: ", error);
+      } finally {
+        setLoading(false);
+      }
     };
-
     getMapZipList();
   }, []);
+
+  if (loading) {
+    return (
+      <section className="w-full p-8 bg-gray-100 text-center">
+        <h2 className="pb-8">맛집 목록</h2>
+        <div>데이터를 불러오는 중입니다...</div>
+      </section>
+    );
+  }
 
   return (
     <section className="w-full p-8 bg-gray-100">
