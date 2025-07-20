c23d33e49abb610163405300d93d8f50b5680cba
Kwonyeojiny <kangagi0009@naver.com>
2025-07-20 17:12:23 +0900

    feat: 에러 처리

--- src/components/MatZipList.tsx
+++ src/components/MatZipList.tsx
@@ -6,14 +6,16 @@ import type { MatZip } from "../types/MatZip";
 const MatZipList = () => {
   const [lists, setLists] = useState<MatZip[]>([]);
   const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
     const getMapZipList = async () => {
       try {
         const data = await getMatZipPlace();
         setLists(data);
-      } catch (error) {
-        console.error("맛집 목록을 가져오는 데 실패했습니다: ", error);
+      } catch (err) {
+        console.error("맛집 목록을 가져오는 데 실패했습니다: ", err);
+        setError("맛집 정보를 불러오는 중 문제가 발생했습니다.");
       } finally {
         setLoading(false);
       }
@@ -26,6 +28,15 @@ const MatZipList = () => {
       <section className="w-full p-8 bg-gray-100 text-center">
         <h2 className="pb-8">맛집 목록</h2>
         <div>데이터를 불러오는 중입니다...</div>
+      </section>
+    );
+  }
+
+  if (error) {
+    return (
+      <section className="w-full p-8 bg-gray-100 text-center">
+        <h2 className="pb-8">맛집 목록</h2>
+        <div>{error}</div>
       </section>
     );
   }
