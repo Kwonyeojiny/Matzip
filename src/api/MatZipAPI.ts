ffe95b3d10c54b0bf5644d81297ce6ab61e6e1fe
Kwonyeojiny <kangagi0009@naver.com>
2025-07-20 16:39:56 +0900

    feat: 찜한 맛집 api 연결

--- src/api/MatZipAPI.ts
+++ src/api/MatZipAPI.ts
@@ -13,3 +13,13 @@ export const getMatZipPlace = async () => {
     return [];
   }
 };
+
+export const getMatZipPickPlace = async () => {
+  try {
+    const response = await baseApi.get("/users/places");
+    return response.data.places;
+  } catch (error) {
+    console.error("찜한 맛집 정보를 가져오는 데 실패했습니다: ", error);
+    return [];
+  }
+};
--- src/components/MatZipPickList.tsx
+++ src/components/MatZipPickList.tsx
@@ -1,14 +1,27 @@
+import { useEffect, useState } from "react";
+import type { MatZip } from "../types/MatZip";
+import { getMatZipPickPlace } from "../api/MatZipAPI";
 import MatZipCard from "./MatZipCard";
 
 const MatZipPickList = () => {
+  const [lists, setLists] = useState<MatZip[]>([]);
+
+  useEffect(() => {
+    const getMapZipList = async () => {
+      const data = await getMatZipPickPlace();
+      setLists(data);
+    };
+
+    getMapZipList();
+  }, []);
+
   return (
     <section className="w-full p-8 bg-gray-100">
       <h2 className="pb-8 text-center">찜한 맛집</h2>
-      <div className="flex justify-around">
-        <MatZipCard />
-        <MatZipCard />
-        <MatZipCard />
-        <MatZipCard />
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
+        {lists.map((list) => (
+          <MatZipCard key={list.id} image={list.image} />
+        ))}
       </div>
     </section>
   );
